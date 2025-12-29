ec18f9fd755e8f24c6726c6c8de333c5780c7003
7Bhil <7bhilal.chitou7@gmail.com>
2025-12-29 07:16:54 +0100

    installation de bases

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ping
-
-urlpatterns = [
-    path('ping/', ping),
-]
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-]
